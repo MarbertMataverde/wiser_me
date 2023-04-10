1b433d61497bcfc781a4885df9b8c5087042d803
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-04-10 12:31:37 +0800

    flutter riverpod initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wiser_me/core/layout/responsive_layout.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
@@ -9,8 +11,51 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Initial Commit'),
+    return const MaterialApp(
+      home: ResponsiveLayout(
+        phone: Phone(),
+        tablet: Tablet(),
+        desktop: Desktop(),
+      ),
+    );
+  }
+}
+
+class Phone extends StatelessWidget {
+  const Phone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('P H O N E'),
+      ),
+    );
+  }
+}
+
+class Tablet extends StatelessWidget {
+  const Tablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('T A B L E T'),
+      ),
+    );
+  }
+}
+
+class Desktop extends StatelessWidget {
+  const Desktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('D E S K T O P'),
+      ),
     );
   }
 }
